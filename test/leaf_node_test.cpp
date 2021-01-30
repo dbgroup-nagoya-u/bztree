e6554bd7a9d0e4e5b29113f0b130a93732b5542a
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-01-30 16:23:52 +0900

    add uint64_t write test

--- test/leaf_node_test.cpp
+++ test/leaf_node_test.cpp
@@ -48,9 +48,9 @@ TEST_F(LeafNodeFixture, New_EmptyNode_CorrectlyInitialized)
 
 TEST_F(LeafNodeFixture, Write_StringValues_MetadataCorrectlyUpdated)
 {
-  auto str_key = "123", str_payload = "4567";
+  auto key = "123", payload = "4567";
   auto key_length = 4, payload_length = 5;
-  auto [rc, status] = node->Write(CastToBytePtr(str_key), key_length, CastToBytePtr(str_payload),
+  auto [rc, status] = node->Write(CastToBytePtr(key), key_length, CastToBytePtr(payload),
                                   payload_length, kIndexEpoch, pool.get());
   auto rec_count = 1, index = 0, block_size = key_length + payload_length;
 
@@ -66,9 +66,9 @@ TEST_F(LeafNodeFixture, Write_StringValues_MetadataCorrectlyUpdated)
   EXPECT_EQ(block_size, status.GetBlockSize());
   EXPECT_EQ(0, status.GetDeletedSize());
 
-  str_key = "test", str_payload = "value";
+  key = "test", payload = "value";
   key_length = 6, payload_length = 6;
-  std::tie(rc, status) = node->Write(CastToBytePtr(str_key), key_length, CastToBytePtr(str_payload),
+  std::tie(rc, status) = node->Write(CastToBytePtr(key), key_length, CastToBytePtr(payload),
                                      payload_length, kIndexEpoch, pool.get());
   ++rec_count;
   ++index;
@@ -114,6 +114,46 @@ TEST_F(LeafNodeFixture, Write_StringValues_ReadWrittenValue)
   std::tie(rc, u_ptr) = node->Read(CastToBytePtr("unknown"), CompareAsCString{});
 
   ASSERT_EQ(BaseNode::NodeReturnCode::kKeyNotExist, rc);
+}
+
+TEST_F(LeafNodeFixture, Write_UIntValues_MetadataCorrectlyUpdated)
+{
+  auto key = 123UL, payload = 4567UL;
+  auto key_length = kWordLength, payload_length = kWordLength;
+  auto [rc, status] = node->Write(CastToBytePtr(&key), key_length, CastToBytePtr(&payload),
+                                  payload_length, kIndexEpoch, pool.get());
+  auto rec_count = 1UL, index = 0UL, block_size = key_length + payload_length;
+
+  ASSERT_EQ(BaseNode::NodeReturnCode::kSuccess, rc);
+  EXPECT_EQ(rec_count, node->GetRecordCount());
+  EXPECT_FALSE(node->IsFrozen());
+  EXPECT_TRUE(node->RecordIsVisible(index));
+  EXPECT_FALSE(node->RecordIsDeleted(index));
+  EXPECT_EQ(key_length, node->GetKeyLength(index));
+  EXPECT_EQ(payload_length, node->GetPayloadLength(index));
+  EXPECT_FALSE(status.IsFrozen());
+  EXPECT_EQ(rec_count, status.GetRecordCount());
+  EXPECT_EQ(block_size, status.GetBlockSize());
+  EXPECT_EQ(0, status.GetDeletedSize());
+
+  key = 890, payload = 1234;
+  std::tie(rc, status) = node->Write(CastToBytePtr(&key), key_length, CastToBytePtr(&payload),
+                                     payload_length, kIndexEpoch, pool.get());
+  ++rec_count;
+  ++index;
+  block_size += key_length + payload_length;
+
+  ASSERT_EQ(BaseNode::NodeReturnCode::kSuccess, rc);
+  EXPECT_EQ(rec_count, node->GetRecordCount());
+  EXPECT_FALSE(node->IsFrozen());
+  EXPECT_TRUE(node->RecordIsVisible(index));
+  EXPECT_FALSE(node->RecordIsDeleted(index));
+  EXPECT_EQ(key_length, node->GetKeyLength(index));
+  EXPECT_EQ(payload_length, node->GetPayloadLength(index));
+  EXPECT_FALSE(status.IsFrozen());
+  EXPECT_EQ(rec_count, status.GetRecordCount());
+  EXPECT_EQ(block_size, status.GetBlockSize());
+  EXPECT_EQ(0, status.GetDeletedSize());
 }
 
 }  // namespace bztree
