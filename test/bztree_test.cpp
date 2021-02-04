f0665d8ef77fec07de0f012e10d94e8e2da34784
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-02-04 10:52:08 +0900

    add fundamental structure for bztree test

---
+++ test/bztree_test.cpp
@@ -0,0 +1,96 @@
+// Copyright (c) DB Group, Nagoya University. All rights reserved.
+// Licensed under the MIT license.
+
+#include "bztree.hpp"
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+using std::byte;
+
+namespace bztree
+{
+static constexpr size_t kDefaultNodeSize = 256;
+static constexpr size_t kDefaultBlockSizeThreshold = 256;
+static constexpr size_t kDefaultDeletedSizeThreshold = 256;
+static constexpr size_t kDefaultMinNodeSizeThreshold = 128;
+static constexpr size_t kIndexEpoch = 0;
+static constexpr size_t kKeyNumForTest = 100;
+
+class BzTreeFixture : public testing::Test
+{
+ public:
+  uint64_t keys[kKeyNumForTest];
+  uint64_t* key_ptrs[kKeyNumForTest];
+  uint64_t key_lengths[kKeyNumForTest];
+  uint64_t payloads[kKeyNumForTest];
+  uint64_t* payload_ptrs[kKeyNumForTest];
+  uint64_t payload_lengths[kKeyNumForTest];
+  uint64_t key_null = 0;  // null key must have 8 bytes to fill a node
+  uint64_t* key_null_ptr = &key_null;
+  size_t key_length_null = kWordLength;  // null key must have 8 bytes to fill a node
+  uint64_t payload_null = 0;             // null payload must have 8 bytes to fill a node
+  uint64_t* payload_null_ptr = &payload_null;
+  size_t payload_length_null = kWordLength;  // null payload must have 8 bytes to fill a node
+
+  CompareAsUInt64 comp{};
+
+ protected:
+  void
+  SetUp() override
+  {
+    pmwcas::InitLibrary(pmwcas::DefaultAllocator::Create, pmwcas::DefaultAllocator::Destroy,
+                        pmwcas::LinuxEnvironment::Create, pmwcas::LinuxEnvironment::Destroy);
+
+    for (uint64_t index = 0; index < kKeyNumForTest; index++) {
+      keys[index] = index;
+      key_ptrs[index] = &keys[index];
+      key_lengths[index] = kWordLength;
+      payloads[index] = index;
+      payload_ptrs[index] = &payloads[index];
+      payload_lengths[index] = kWordLength;
+    }
+  }
+
+  void
+  TearDown() override
+  {
+    pmwcas::Thread::ClearRegistry();
+  }
+
+  constexpr uint64_t
+  CastToValue(const void* target_addr)
+  {
+    return *BitCast<uint64_t*>(target_addr);
+  }
+
+  void
+  WriteNullKey(  //
+      LeafNode* target_node,
+      const size_t write_num)
+  {
+    for (size_t index = 0; index < write_num; ++index) {
+      //
+    }
+  }
+
+  void
+  WriteOrderedKeys(  //
+      LeafNode* target_node,
+      const size_t begin_index,
+      const size_t end_index)
+  {
+    assert(end_index < kKeyNumForTest);
+
+    for (size_t index = begin_index; index <= end_index; ++index) {
+      auto key_ptr = key_ptrs[index];
+      auto key_length = key_lengths[index];
+      auto payload_ptr = payload_ptrs[index];
+      auto payload_length = payload_lengths[index];
+      //
+    }
+  }
+};
+
+}  // namespace bztree
