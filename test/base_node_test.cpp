55fa7024b0805b32155594dbd56c689060426453
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-03-16 15:40:22 +0900

    modify base node tests

--- test/base_node_test.cpp
+++ test/base_node_test.cpp
@@ -1,252 +1,233 @@
-// // Copyright (c) DB Group, Nagoya University. All rights reserved.
-// // Licensed under the MIT license.
+// Copyright (c) DB Group, Nagoya University. All rights reserved.
+// Licensed under the MIT license.
 
-// #include "bztree/components/base_node.hpp"
+#include "bztree/components/base_node.hpp"
 
-// #include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
-// #include <memory>
+#include <memory>
 
-// #include "bztree/components/leaf_node.hpp"
+#include "bztree/components/leaf_node.hpp"
 
-// using std::byte;
+using std::byte;
 
-// namespace dbgroup::index::bztree
-// {
-// static constexpr size_t kNodeSize = 256;
-// static constexpr size_t kIndexEpoch = 0;
-// static constexpr size_t kKeyNumForTest = 100;
+namespace dbgroup::index::bztree
+{
+using Key = uint64_t;
+using Payload = uint64_t;
+using Record_t = Record<Key, Payload>;
+using BaseNode_t = BaseNode<Key, Payload>;
+using LeafNode_t = LeafNode<Key, Payload>;
+using NodeReturnCode = BaseNode<Key, Payload>::NodeReturnCode;
+using KeyExistence = BaseNode<Key, Payload>::KeyExistence;
 
-// class BaseNodeFixture : public testing::Test
-// {
-//  public:
-//   uint64_t keys[kKeyNumForTest];
-//   uint64_t* key_ptrs[kKeyNumForTest];
-//   uint64_t key_lengths[kKeyNumForTest];
-//   uint64_t payloads[kKeyNumForTest];
-//   uint64_t* payload_ptrs[kKeyNumForTest];
-//   uint64_t payload_lengths[kKeyNumForTest];
-//   uint64_t key_null = 0;  // null key must have 8 bytes to fill a node
-//   uint64_t* key_null_ptr = &key_null;
-//   size_t key_length_null = kWordLength;  // null key must have 8 bytes to fill a node
-//   uint64_t payload_null = 0;             // null payload must have 8 bytes to fill a node
-//   uint64_t* payload_null_ptr = &payload_null;
-//   size_t payload_length_null = kWordLength;  // null payload must have 8 bytes to fill a node
+static constexpr size_t kNodeSize = 256;
+static constexpr size_t kIndexEpoch = 0;
+static constexpr size_t kKeyNumForTest = 10000;
+static constexpr size_t kKeyLength = sizeof(Key);
+static constexpr size_t kPayloadLength = sizeof(Payload);
+static constexpr size_t kRecordLength = kKeyLength + kPayloadLength;
 
-//   CompareAsUInt64 comp{};
+class BaseNodeFixture : public testing::Test
+{
+ public:
+  Key keys[kKeyNumForTest];
+  Payload payloads[kKeyNumForTest];
+  Key key_null = 0;          // null key must have 8 bytes to fill a node
+  Payload payload_null = 0;  // null payload must have 8 bytes to fill a node
 
-//  protected:
-//   void
-//   SetUp() override
-//   {
-//     for (uint64_t index = 0; index < kKeyNumForTest; index++) {
-//       keys[index] = index;
-//       key_ptrs[index] = &keys[index];
-//       key_lengths[index] = kWordLength;
-//       payloads[index] = index;
-//       payload_ptrs[index] = &payloads[index];
-//       payload_lengths[index] = kWordLength;
-//     }
-//   }
+  std::unique_ptr<LeafNode_t> node;
 
-//   void
-//   TearDown() override
-//   {
-//   }
+ protected:
+  void
+  SetUp() override
+  {
+    node.reset(LeafNode_t::CreateEmptyNode(kNodeSize));
 
-//   constexpr uint64_t
-//   CastToValue(const void* target_addr)
-//   {
-//     return *BitCast<uint64_t*>(target_addr);
-//   }
+    for (size_t index = 0; index < kKeyNumForTest; index++) {
+      keys[index] = index + 1;
+      payloads[index] = index + 1;
+    }
+  }
 
-//   void
-//   WriteNullKey(  //
-//       LeafNode* target_node,
-//       const size_t write_num)
-//   {
-//     for (size_t index = 0; index < write_num; ++index) {
-//       target_node->Write(key_null_ptr, key_length_null, payload_null_ptr, payload_length_null,
-//                          kIndexEpoch);
-//     }
-//   }
+  void
+  TearDown() override
+  {
+  }
 
-//   void
-//   WriteOrderedKeys(  //
-//       LeafNode* target_node,
-//       const size_t begin_index,
-//       const size_t end_index)
-//   {
-//     assert(end_index < kKeyNumForTest);
+  void
+  WriteNullKey(  //
+      LeafNode_t* target_node,
+      const size_t write_num)
+  {
+    for (size_t index = 0; index < write_num; ++index) {
+      target_node->Write(key_null, kKeyLength, payload_null, kPayloadLength);
+    }
+  }
 
-//     for (size_t index = begin_index; index <= end_index; ++index) {
-//       auto key_ptr = key_ptrs[index];
-//       auto key_length = key_lengths[index];
-//       auto payload_ptr = payload_ptrs[index];
-//       auto payload_length = payload_lengths[index];
-//       target_node->Write(key_ptr, key_length, payload_ptr, payload_length, kIndexEpoch);
-//     }
-//   }
+  void
+  WriteOrderedKeys(  //
+      LeafNode_t* target_node,
+      const size_t begin_index,
+      const size_t end_index)
+  {
+    assert(end_index < kKeyNumForTest);
 
-//   std::unique_ptr<LeafNode>
-//   CreateSortedLeafNodeWithOrderedKeys(  //
-//       const size_t begin_index,
-//       const size_t end_index)
-//   {
-//     auto tmp_leaf_node = LeafNode::CreateEmptyNode(kNodeSize);
-//     WriteOrderedKeys(tmp_leaf_node, begin_index, end_index);
-//     auto tmp_meta = tmp_leaf_node->GatherSortedLiveMetadata(comp);
-//     tmp_leaf_node = LeafNode::Consolidate(tmp_leaf_node, tmp_meta);
-//     return std::unique_ptr<LeafNode>(tmp_leaf_node);
-//   }
-// };
+    for (size_t index = begin_index; index <= end_index; ++index) {
+      target_node->Write(keys[index], kKeyLength, payloads[index], kPayloadLength);
+    }
+  }
 
-// TEST_F(BaseNodeFixture, New_EmptyNode_CorrectlyInitialized)
-// {
-//   auto node = std::unique_ptr<BaseNode>(BaseNode::CreateEmptyNode(kNodeSize, true));
-//   auto status = *BitCast<StatusWord*>(ShiftAddress(node.get(), kWordLength));
+  LeafNode_t*
+  CreateSortedLeafNodeWithOrderedKeys(  //
+      const size_t begin_index,
+      const size_t end_index)
+  {
+    auto tmp_leaf_node = LeafNode_t::CreateEmptyNode(kNodeSize);
+    WriteOrderedKeys(tmp_leaf_node, begin_index, end_index);
+    auto tmp_meta = tmp_leaf_node->GatherSortedLiveMetadata();
+    return LeafNode_t::Consolidate(tmp_leaf_node, tmp_meta);
+  }
+};
 
-//   EXPECT_EQ(kNodeSize, node->GetNodeSize());
-//   EXPECT_EQ(0, node->GetSortedCount());
-//   EXPECT_EQ(status, node->GetStatusWord());
-// }
+TEST_F(BaseNodeFixture, New_EmptyNode_CorrectlyInitialized)
+{
+  auto status = *BitCast<StatusWord*>(ShiftAddress(node.get(), kWordLength));
 
-// TEST_F(BaseNodeFixture, Freeze_NotFrozenNode_FreezeNode)
-// {
-//   auto node = std::unique_ptr<BaseNode>(BaseNode::CreateEmptyNode(kNodeSize, true));
+  EXPECT_EQ(kNodeSize, node->GetNodeSize());
+  EXPECT_EQ(0, node->GetSortedCount());
+  EXPECT_EQ(status, node->GetStatusWord());
+}
 
-//   auto rc = node->Freeze();
-//   auto status = node->GetStatusWord();
+TEST_F(BaseNodeFixture, Freeze_NotFrozenNode_FreezeNode)
+{
+  auto rc = node->Freeze();
+  auto status = node->GetStatusWord();
 
-//   EXPECT_EQ(BaseNode::NodeReturnCode::kSuccess, rc);
-//   EXPECT_TRUE(status.IsFrozen());
-// }
+  EXPECT_EQ(NodeReturnCode::kSuccess, rc);
+  EXPECT_TRUE(status.IsFrozen());
+}
 
-// TEST_F(BaseNodeFixture, Freeze_FrozenNode_FreezeFailed)
-// {
-//   auto node = std::unique_ptr<BaseNode>(BaseNode::CreateEmptyNode(kNodeSize, true));
+TEST_F(BaseNodeFixture, Freeze_FrozenNode_FreezeFailed)
+{
+  node->Freeze();
+  auto rc = node->Freeze();
+  auto status = node->GetStatusWord();
 
-//   node->Freeze();
-//   auto rc = node->Freeze();
-//   auto status = node->GetStatusWord();
+  EXPECT_EQ(NodeReturnCode::kFrozen, rc);
+  EXPECT_TRUE(status.IsFrozen());
+}
 
-//   EXPECT_EQ(BaseNode::NodeReturnCode::kFrozen, rc);
-//   EXPECT_TRUE(status.IsFrozen());
-// }
+TEST_F(BaseNodeFixture, SearchSortedMeta_SearchPresentKeyWithClosedRange_FindKeyIndex)
+{
+  node.reset(CreateSortedLeafNodeWithOrderedKeys(0, 9));
+  auto target_index = 7;
+  auto target_key = keys[target_index];
 
-// TEST_F(BaseNodeFixture, SearchSortedMeta_SearchPresentKeyWithClosedRange_FindKeyIndex)
-// {
-//   auto node = CreateSortedLeafNodeWithOrderedKeys(1, 10);
-//   auto target_index = 7;
-//   auto target_key = key_ptrs[target_index + 1];
+  auto [rc, index] = node->SearchSortedMetadata(target_key, true);
 
-//   auto [rc, index] = node->SearchSortedMetadata(target_key, true, comp);
+  EXPECT_EQ(KeyExistence::kExist, rc);
+  EXPECT_EQ(target_index, index);
 
-//   EXPECT_EQ(BaseNode::KeyExistence::kExist, rc);
-//   EXPECT_EQ(target_index, index);
+  target_index = 3;
+  target_key = keys[target_index];
 
-//   target_index = 3;
-//   target_key = key_ptrs[target_index + 1];
+  std::tie(rc, index) = node->SearchSortedMetadata(target_key, true);
 
-//   std::tie(rc, index) = node->SearchSortedMetadata(target_key, true, comp);
+  EXPECT_EQ(KeyExistence::kExist, rc);
+  EXPECT_EQ(target_index, index);
+}
 
-//   EXPECT_EQ(BaseNode::KeyExistence::kExist, rc);
-//   EXPECT_EQ(target_index, index);
-// }
+TEST_F(BaseNodeFixture, SearchSortedMeta_SearchPresentKeyWithOpenedRange_FindNextIndex)
+{
+  node.reset(CreateSortedLeafNodeWithOrderedKeys(0, 9));
+  auto target_index = 7;
+  auto target_key = keys[target_index];
 
-// TEST_F(BaseNodeFixture, SearchSortedMeta_SearchPresentKeyWithOpenedRange_FindNextIndex)
-// {
-//   auto node = CreateSortedLeafNodeWithOrderedKeys(1, 10);
-//   auto target_index = 7;
-//   auto target_key = key_ptrs[target_index];
+  auto [rc, index] = node->SearchSortedMetadata(target_key, false);
 
-//   auto [rc, index] = node->SearchSortedMetadata(target_key, false, comp);
+  EXPECT_EQ(KeyExistence::kExist, rc);
+  EXPECT_EQ(target_index + 1, index);
 
-//   EXPECT_EQ(BaseNode::KeyExistence::kExist, rc);
-//   EXPECT_EQ(target_index, index);
+  target_index = 3;
+  target_key = keys[target_index];
 
-//   target_index = 3;
-//   target_key = key_ptrs[target_index];
+  std::tie(rc, index) = node->SearchSortedMetadata(target_key, false);
 
-//   std::tie(rc, index) = node->SearchSortedMetadata(target_key, false, comp);
+  EXPECT_EQ(KeyExistence::kExist, rc);
+  EXPECT_EQ(target_index + 1, index);
+}
 
-//   EXPECT_EQ(BaseNode::KeyExistence::kExist, rc);
-//   EXPECT_EQ(target_index, index);
-// }
+TEST_F(BaseNodeFixture, SearchSortedMeta_SearchNotPresentKey_FindNextIndex)
+{
+  // prepare a target node
+  auto tmp_node = std::unique_ptr<LeafNode_t>(LeafNode_t::CreateEmptyNode(kNodeSize));
+  tmp_node->Write(keys[1], kKeyLength, payloads[1], kPayloadLength);
+  tmp_node->Write(keys[2], kKeyLength, payloads[2], kPayloadLength);
+  tmp_node->Write(keys[4], kKeyLength, payloads[4], kPayloadLength);
+  tmp_node->Write(keys[5], kKeyLength, payloads[5], kPayloadLength);
+  tmp_node->Write(keys[7], kKeyLength, payloads[7], kPayloadLength);
+  tmp_node->Write(keys[8], kKeyLength, payloads[8], kPayloadLength);
+  auto tmp_meta = tmp_node->GatherSortedLiveMetadata();
 
-// TEST_F(BaseNodeFixture, SearchSortedMeta_SearchNotPresentKey_FindNextIndex)
-// {
-//   // prepare a target node
-//   auto tmp_node = std::unique_ptr<LeafNode>(LeafNode::CreateEmptyNode(kNodeSize));
-//   tmp_node->Write(key_ptrs[1], key_lengths[1], payload_ptrs[1], payload_lengths[1], kIndexEpoch);
-//   tmp_node->Write(key_ptrs[2], key_lengths[2], payload_ptrs[2], payload_lengths[2], kIndexEpoch);
-//   tmp_node->Write(key_ptrs[4], key_lengths[4], payload_ptrs[4], payload_lengths[4], kIndexEpoch);
-//   tmp_node->Write(key_ptrs[5], key_lengths[5], payload_ptrs[5], payload_lengths[5], kIndexEpoch);
-//   tmp_node->Write(key_ptrs[7], key_lengths[7], payload_ptrs[7], payload_lengths[7], kIndexEpoch);
-//   tmp_node->Write(key_ptrs[8], key_lengths[8], payload_ptrs[8], payload_lengths[8], kIndexEpoch);
-//   auto tmp_meta = tmp_node->GatherSortedLiveMetadata(comp);
-//   auto node = std::unique_ptr<LeafNode>(LeafNode::Consolidate(tmp_node.get(), tmp_meta));
+  node.reset(LeafNode_t::Consolidate(tmp_node.get(), tmp_meta));
 
-//   // perform tests
-//   auto target_index = 2;
-//   auto target_key = key_ptrs[3];
+  // perform tests
+  auto target_index = 3;
+  auto target_key = keys[target_index];
 
-//   auto [rc, index] = node->SearchSortedMetadata(target_key, true, comp);
+  auto [rc, index] = node->SearchSortedMetadata(target_key, true);
 
-//   EXPECT_EQ(BaseNode::KeyExistence::kNotExist, rc);
-//   EXPECT_EQ(target_index, index);
+  EXPECT_EQ(KeyExistence::kNotExist, rc);
+  EXPECT_EQ(target_index - 1, index);
 
-//   target_index = 4;
-//   target_key = key_ptrs[6];
+  target_index = 6;
+  target_key = keys[target_index];
 
-//   std::tie(rc, index) = node->SearchSortedMetadata(target_key, false, comp);
+  std::tie(rc, index) = node->SearchSortedMetadata(target_key, false);
 
-//   EXPECT_EQ(BaseNode::KeyExistence::kNotExist, rc);
-//   EXPECT_EQ(target_index, index);
-// }
+  EXPECT_EQ(KeyExistence::kNotExist, rc);
+  EXPECT_EQ(target_index - 2, index);
+}
 
-// TEST_F(BaseNodeFixture, SearchSortedMeta_SearchDeletedKey_FindKeyIndex)
-// {
-//   auto node = CreateSortedLeafNodeWithOrderedKeys(1, 10);
-//   auto target_index = 7;
-//   auto target_key = key_ptrs[target_index + 1];
-//   auto target_key_length = key_lengths[target_index + 1];
-//   node->Delete(target_key, target_key_length, comp);
+TEST_F(BaseNodeFixture, SearchSortedMeta_SearchDeletedKey_FindKeyIndex)
+{
+  node.reset(CreateSortedLeafNodeWithOrderedKeys(0, 9));
+  auto target_index = 7;
+  auto target_key = keys[target_index];
+  node->Delete(target_key, kKeyLength);
 
-//   auto [rc, index] = node->SearchSortedMetadata(target_key, true, comp);
+  auto [rc, index] = node->SearchSortedMetadata(target_key, true);
 
-//   EXPECT_EQ(BaseNode::KeyExistence::kDeleted, rc);
-//   EXPECT_EQ(target_index, index);
+  EXPECT_EQ(KeyExistence::kDeleted, rc);
+  EXPECT_EQ(target_index, index);
 
-//   target_index = 3;
-//   target_key = key_ptrs[target_index + 1];
-//   target_key_length = key_lengths[target_index + 1];
-//   node->Delete(target_key, target_key_length, comp);
+  target_index = 3;
+  target_key = keys[target_index];
+  node->Delete(target_key, kKeyLength);
 
-//   std::tie(rc, index) = node->SearchSortedMetadata(target_key, false, comp);
+  std::tie(rc, index) = node->SearchSortedMetadata(target_key, false);
 
-//   EXPECT_EQ(BaseNode::KeyExistence::kDeleted, rc);
-//   EXPECT_EQ(target_index, index);
-// }
+  EXPECT_EQ(KeyExistence::kDeleted, rc);
+  EXPECT_EQ(target_index, index);
+}
 
-// TEST_F(BaseNodeFixture, SearchSortedMeta_SearchOutOfNodeKey_FindBorderIndex)
-// {
-//   auto node = CreateSortedLeafNodeWithOrderedKeys(1, 10);
-//   auto target_index = 0;
-//   auto target_key = key_ptrs[target_index];
+TEST_F(BaseNodeFixture, SearchSortedMeta_SearchOutOfNodeKey_FindBorderIndex)
+{
+  node.reset(CreateSortedLeafNodeWithOrderedKeys(0, 9));
 
-//   auto [rc, index] = node->SearchSortedMetadata(target_key, true, comp);
+  auto [rc, index] = node->SearchSortedMetadata(key_null, true);
 
-//   EXPECT_EQ(BaseNode::KeyExistence::kNotExist, rc);
-//   EXPECT_EQ(target_index, index);
+  EXPECT_EQ(KeyExistence::kNotExist, rc);
+  EXPECT_EQ(0, index);
 
-//   target_index = 10;
-//   target_key = key_ptrs[target_index + 1];
+  auto target_index = 10;
+  auto target_key = keys[target_index];
 
-//   std::tie(rc, index) = node->SearchSortedMetadata(target_key, false, comp);
+  std::tie(rc, index) = node->SearchSortedMetadata(target_key, false);
 
-//   EXPECT_EQ(BaseNode::KeyExistence::kNotExist, rc);
-//   EXPECT_EQ(target_index, index);
-// }
+  EXPECT_EQ(KeyExistence::kNotExist, rc);
+  EXPECT_EQ(target_index, index);
+}
 
-// }  // namespace dbgroup::index::bztree
+}  // namespace dbgroup::index::bztree
