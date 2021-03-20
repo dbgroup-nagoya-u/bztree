58f4ad8cd334f0b5051cc256ff2be3b93e7dc317
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-03-20 15:56:31 +0900

    update GC

--- include/bztree/bztree.hpp
+++ include/bztree/bztree.hpp
@@ -608,7 +608,7 @@ class BzTree
         max_deleted_size_{max_deleted_size},
         max_merged_size_{max_merged_size},
         index_epoch_{0},
-        gc_{1024, 1000}
+        gc_{1000}
   {
     // initialize a tree structure: one internal node with one leaf node
     const auto root_node = InternalNode_t::CreateInitialRoot(node_size_);
