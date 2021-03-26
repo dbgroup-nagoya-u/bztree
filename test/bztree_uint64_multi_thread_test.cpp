912f166acf3e88ca343a7a88868d64e23d3bf9ed
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-03-26 17:05:15 +0900

    add BzTree multi-thread test

---
+++ test/bztree_uint64_multi_thread_test.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) DB Group, Nagoya University. All rights reserved.
+// Licensed under the MIT license.
+
+#include "bztree/components/common.hpp"
+
+using Key = uint64_t;
+using Payload = uint64_t;
+using Compare = std::less<Key>;
+using PayloadComparator = std::less<Payload>;
+constexpr size_t kKeyLength = sizeof(Key);
+constexpr size_t kPayloadLength = sizeof(Payload);
+
+#include "bztree_multi_thread_test.hpp"
+
+namespace dbgroup::index::bztree
+{
+void
+BzTreeFixture::SetUp()
+{
+  for (size_t index = 0; index < kKeyNumForTest; ++index) {
+    keys[index] = index + 1;
+    payloads[index] = index + 1;
+  }
+}
+
+void
+BzTreeFixture::TearDown()
+{
+}
+
+}  // namespace dbgroup::index::bztree
